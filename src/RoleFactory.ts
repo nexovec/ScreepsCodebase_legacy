00e26d861a20406ded8f2fe0fbf762ee45abea50
nexovec <hraccesky@gmail.com>
2020-10-08 23:11:42 +0200

    working RoleHauler

--- src/RoleFactory.ts
+++ src/RoleFactory.ts
@@ -1,10 +1,14 @@
 import { RoleHarvester } from "./RoleHarvester";
+import { RoleHauler } from "RoleHauler";
 import { RoleUpgrader } from "RoleUpgrader";
 export class RoleFactory {
   public constructor() {
     return;
   }
-  public getRole(creepName: string): RoleUpgrader {
-    return new RoleUpgrader(creepName);
+  //   public getRole(creepName: string): RoleHarvester {
+  //     return new RoleHarvester(creepName);
+  //   }
+  public getRole(creepName: string): RoleHauler {
+    return new RoleHauler(creepName);
   }
 }
---
+++ src/RoleHauler.ts
@@ -0,0 +1,24 @@
+export class RoleHauler {
+  private creepName: string;
+  public constructor(creepName: string) {
+    this.creepName = creepName;
+    return;
+  }
+  public run(): void {
+    const c = Game.creeps[this.creepName];
+    const resources = c.room.find(FIND_DROPPED_RESOURCES);
+    const tombstones = c.room.find(FIND_TOMBSTONES);
+    const storages = c.room.find(FIND_MY_STRUCTURES);
+    if (tombstones.length !== 0) {
+      if (c.withdraw(tombstones[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        c.moveTo(tombstones[0]);
+      }
+    } else if (resources.length !== 0) {
+      if (c.pickup(resources[0]) === ERR_NOT_IN_RANGE) c.moveTo(resources[0]);
+    } else {
+      if (c.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) c.moveTo(Game.spawns.Spawn1);
+    }
+    if (c.store.getFreeCapacity() === 0)
+      if (c.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) c.moveTo(Game.spawns.Spawn1);
+  }
+}

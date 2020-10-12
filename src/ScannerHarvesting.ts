04e635d384b08542e18b130dc3602a3c8fb3de13
nexovec <hraccesky@gmail.com>
2020-10-12 18:57:31 +0200

    enhanced, native freeSlots computation

--- src/ScannerHarvesting.ts
+++ src/ScannerHarvesting.ts
@@ -1,23 +1,26 @@
 import { WrapperSource } from "./WrapperSource";
 export class ScannerHarvesting {
   public rooms: Room[];
+  private wrappers: WrapperSource[];
+  public freeSlots: number;
   public constructor(rooms: Room[]) {
     this.rooms = rooms;
-    const wrappers: WrapperSource[] = [];
+    this.wrappers = [];
     for (const room of this.rooms) {
       for (const source of room.find(FIND_SOURCES)) {
-        wrappers.push(new WrapperSource(source));
+        this.wrappers.push(new WrapperSource(source));
       }
     }
-    console.log(
-      "total free slots: " +
-        Number(
-          _.reduce(
-            _.map(wrappers, val => val.freeSlots),
-            (a: number, b: number) => a + b
-          )
-        ).toString()
-    );
+    // console.log(
+    //   "total free slots: " +
+    //     Number(
+    //       _.reduce(
+    //         _.map(this.wrappers, val => val.freeSlots), // replace with native map/reduce
+    //         (a: number, b: number) => a + b
+    //       )
+    //     ).toString()
+    // );
+    this.freeSlots = this.wrappers.map(val => val.freeSlots).reduce((a, b) => a + b);
     return;
   }
   public getSources(): Source[] {

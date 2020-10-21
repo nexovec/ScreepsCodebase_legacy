9711abce5a1716b5588bd69fe0bbb960dd1470cf
nexovec <hraccesky@gmail.com>
2020-10-21 15:35:43 +0200

    I have no life

--- src/Task.ts
+++ src/Task.ts
@@ -2,30 +2,30 @@ import { Colony } from "Colony";
 
 export class Task {
   public colony: Colony;
-  protected predicates: { [index: number]: () => boolean };
-  protected commands: { [index: number]: () => any };
+  protected commands: { [index: number]: () => boolean };
   protected step: number;
-  protected isComplete: boolean;
-  public constructor(
-    commands: { [index: number]: () => any },
-    predicates: { [index: number]: () => boolean },
-    colony: Colony
-  ) {
+  public isComplete: boolean;
+  public constructor(commands: { [index: number]: () => boolean }, colony: Colony) {
     this.commands = commands;
-    this.predicates = predicates;
-    this.step = Object.keys(this.predicates).length - 1;
+    this.step = Object.keys(this.commands).length - 1;
     this.isComplete = false;
     this.colony = colony;
   }
   public resolve(): void {
     console.log(this.isComplete);
     if (this.isComplete) return;
-    while (this.predicates[this.step]()) {
-      this.commands[this.step]();
+    // console.log(this.step);
+    const res: boolean = this.commands[this.step]();
+    // console.log(res);
+    while (res) {
+      // console.log("I've completed a command!");
       if (this.step === 0) {
         this.isComplete = true;
-        break;
-      } else this.step -= 1;
+        return;
+      }
+      this.step -= 1;
+      return;
     }
+    console.log("Your command didn't complete successfully!");
   }
 }
--- src/TaskSpawnWorker.ts
+++ src/TaskSpawnWorker.ts
@@ -2,24 +2,33 @@ import { Colony } from "Colony";
 import { Task } from "Task";
 import { UUID } from "UUID";
 export class TaskSpawnWorker extends Task {
+  public name: string;
   public constructor(colony: Colony) {
     super(
       {
         // commands
         0: () => {
-          const name = "nex#" + new UUID().toString();
-          Game.spawns.Spawn1.spawnCreep(this.colony.bodies.worker.getBody(), name);
-        }
-      },
-      {
-        // predicates
-        0: () => {
           if (this.getEnergy() < this.colony.bodies.worker.getBodyCost()) return false;
-          else return true;
+          if (!Game.creeps[this.name]) {
+            console.log("The creep doesn't yet exist");
+            // return true;
+            const res = Game.spawns.Spawn1.spawnCreep(this.colony.bodies.worker.getBody(), this.name);
+            if (res !== OK) {
+              console.log("spawning wasn't OK");
+              return false;
+            }
+            return false;
+          }
+          if (Game.creeps[this.name].spawning) {
+            console.log("creep " + this.name + "is still being spawned");
+            return false;
+          } else console.log("creep " + this.name + " finished spawning");
+          return true;
         }
       },
       colony
     );
+    this.name = "nex#" + new UUID().toString();
   }
   private getEnergy(): number {
     return Game.spawns.Spawn1.store.energy;

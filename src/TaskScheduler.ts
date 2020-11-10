c697a5cb84a075716c1a92a41f2a37b24f33aa6c
nexovec <hraccesky@gmail.com>
2020-11-10 15:06:26 +0100

    planning ahead

--- src/TaskScheduler.ts
+++ src/TaskScheduler.ts
@@ -2,13 +2,17 @@ import { Colony } from "Colony";
 import { Task } from "tasks/Task";
 export class TaskScheduler {
   public colony: Colony;
+  public tasks: Task[];
   public constructor(colony: Colony) {
     this.colony = colony;
+    this.tasks = [];
   }
   public loop(): void {
+    this.tasks.map(task => task.resolve()); // just adds complexity at this point
     return;
   }
   public schedule(task: Task): void {
+    this.tasks.push(task); // mustn't already contain the task
     return;
   }
 }
--- src/managers/HarvestingManager.ts
+++ src/managers/HarvestingManager.ts
@@ -22,21 +22,32 @@ export class HarvestingManager extends Manager {
     if (Object.keys(Game.creeps).length > Object.keys(this.workingCreeps).length) {
       // single colony only
       this.setIdleCreeps();
+      this.computeSourceSpaces();
       // TODO: schedule tasks for idle creeps
+      if (Game.spawns.Spawn1.room.controller && Game.spawns.Spawn1.room.controller?.level < 3) {
+        // just make 1:1:1 workers for each free slot
+        // when they're filled, go on to upgrade controller
+        // when RCL === 2, start making extensions
+        // when <fixed number> of extensions are complete, fill them up
+        // BodyWorker will return body based on <RCL,extension count, ??>
+        // when they are filled, build roads to sources
+        // when that is done, build a road towards the controller and repair them
+        // when that is done, ensure production of workers and upgrade controller
+        // when RCL === 3, start making extensions and fill them up
+      }
     }
 
     return;
   }
   private computeSourceSpaces(): void {
     for (const source of this.colony.room.find(FIND_SOURCES)) {
-      // for(const position of source.pos.)
       const p = source.pos;
       const t = this.colony.room
         .lookForAtArea("terrain", p.y - 1, p.x - 1, p.y + 1, p.x + 1, true)
         .filter(val => val.terrain !== "wall").length;
       const input = { totalSpaces: t, freeSpaces: t };
       // console.log(JSON.stringify(input));
-      [source.freeSpaces, source.totalSpaces] = [input.freeSpaces, input.totalSpaces];
+      [source.freeSpaces, source.totalSpaces] = [input.freeSpaces, input.totalSpaces]; // might not do what you want
     }
   }
   private setIdleCreeps() {
